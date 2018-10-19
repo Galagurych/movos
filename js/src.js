c78ad8a71ec5edda72fe60a9af1a4ad5bb008763
Sergii Galagurych <ssgalagurich@gmail.com>
2018-10-19 22:43:53 +0300

    everything is ready, not verified

--- js/src.js
+++ js/src.js
@@ -1,1 +1,15 @@
-console.log(document, 'in file')
+var menuOpener = document.getElementById('menu-opener');
+var menu = document.getElementById('menu');
+
+
+menuOpener.addEventListener(
+  'click',
+  function (){
+    if (menuOpener.classList.contains("active")){
+      menuOpener.classList.remove("active");
+      menu.classList.remove("active");
+    }else{
+      menuOpener.classList.add("active");
+      menu.classList.add("active");
+    } 
+})
